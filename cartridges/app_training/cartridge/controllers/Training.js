439d539ffc0c65e1058001fa4d5cc4a343ff62bd
maria-osfdigital <maria.porcina@osf.digital>
2022-03-11 15:00:10 -0300

    finishing module 7 practical exercises
    - use a custom tag(review);
    - creating a basktet javascript controller and using a loop isml;
    - using resource bundles.

--- cartridges/app_training/cartridge/controllers/Training.js
+++ cartridges/app_training/cartridge/controllers/Training.js
@@ -10,4 +10,13 @@ server.get('HelloWorld', function(req, res, next) {
 
 });
 
+server.get('Basket', function(req, res, next){
+    var BasketMgr = require("dw/order/BasketMgr");
+
+    var currentBasket = BasketMgr.getCurrentBasket();
+
+    res.render("training/showBasket", {basket: currentBasket})
+    return next();
+});
+
 module.exports = server.exports();
