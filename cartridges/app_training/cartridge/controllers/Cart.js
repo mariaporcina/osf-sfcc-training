e9b6c9ae24f92637f591ca21e1d75d13da0920b7
maria-osfdigital <maria.porcina@osf.digital>
2022-03-21 13:07:22 -0300

    updating Cart.js
    - retrieving customer's email and site preferences default email
    - retrieving product data
    - setting up info to be able to send the email

--- cartridges/app_training/cartridge/controllers/Cart.js
+++ cartridges/app_training/cartridge/controllers/Cart.js
@@ -6,15 +6,29 @@ server.extend(module.superModule);
 
 server.append('AddProduct', function(req, res, next) {
     var Site = require('dw/system/Site');
-    var currentSite = Site.getCurrent();
+    var Mail = require('dw/net/Mail');
+    var BasketMgr = require('dw/order/BasketMgr');
+    var ProductMgr = require('dw/catalog/ProductMgr');
 
-    var CustomerCDPData = require('dw/customer/CustomerMgr');
-    var teste = CustomerCDPData.getCustomerGroups();
+    var currentSite = Site.getCurrent();
+    var fromEmail = currentSite.getCustomPreferenceValue('customerServiceEmail');
 
     var currentCustomer = req.currentCustomer;
+    var customerEmail = currentCustomer.profile !== undefined ? currentCustomer.profile.email : undefined;
+
+    var currentBasket = BasketMgr.getCurrentBasket();
+    var basketItems = currentBasket.productLineItems;
+    var lastAddedItem = basketItems[basketItems.length-1];
+    var product = ProductMgr.getProduct(lastAddedItem.productID);
+
+    // var mail = new Mail();
+    // mail.addTo(customerEmail);
+    // mail.setFrom(fromEmail);
+    // mail.setSubject('Confirmation of Your Order');
+    // mail.setContent('Product added to your cart');
+    // mail.send();
 
     var viewData = res.getViewData();
-
 
     return next();
 });
