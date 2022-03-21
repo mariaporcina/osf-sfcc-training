32cdca2a53c43bdb9312b9850d004ad31fe584e0
maria-osfdigital <maria.porcina@osf.digital>
2022-03-21 19:42:59 -0300

    finished challenge
    - retrieving all product data and rendering template successfully

--- cartridges/app_training/cartridge/controllers/Cart.js
+++ cartridges/app_training/cartridge/controllers/Cart.js
@@ -8,7 +8,10 @@ server.append('AddProduct', function(req, res, next) {
     var Site = require('dw/system/Site');
     var Mail = require('dw/net/Mail');
     var BasketMgr = require('dw/order/BasketMgr');
-    var ProductMgr = require('dw/catalog/ProductMgr');
+    var HashMap = require('dw/util/HashMap');
+    var Template = require('dw/util/Template');
+    var Resource = require('dw/web/Resource');
+    var URLUtils = require('dw/web/URLUtils');
 
     var currentSite = Site.getCurrent();
     var fromEmail = currentSite.getCustomPreferenceValue('customerServiceEmail');
@@ -19,16 +22,32 @@ server.append('AddProduct', function(req, res, next) {
     var currentBasket = BasketMgr.getCurrentBasket();
     var basketItems = currentBasket.productLineItems;
     var lastAddedItem = basketItems[basketItems.length-1];
-    var product = ProductMgr.getProduct(lastAddedItem.productID);
+    var product = lastAddedItem.product;
 
-    // var mail = new Mail();
-    // mail.addTo(customerEmail);
-    // mail.setFrom(fromEmail);
-    // mail.setSubject('Confirmation of Your Order');
-    // mail.setContent('Product added to your cart');
-    // mail.send();
+    var productPrice = product.getPriceModel();
 
-    var viewData = res.getViewData();
+    var productAttributes = {
+        productName: product.getName(),
+        productURL: URLUtils.https('Product-Show', 'pid', product.ID).toString(),
+        productImage: product.getImage('medium').absURL,
+        productDescription: product.getShortDescription(),
+        productQuantity: lastAddedItem.quantityValue,
+        pricePerUnit: productPrice.pricePerUnit.decimalValue,
+        priceCurrency: productPrice.pricePerUnit.currencyCode
+    }
+
+    var hashMap = new HashMap();
+    hashMap.put('product', productAttributes);
+
+    var template = new Template('cart/mailAddedProduct.isml');
+    var emailContent = template.render(hashMap);
+
+    var mail = new Mail();
+    mail.addTo(customerEmail);
+    mail.setFrom(fromEmail);
+    mail.setSubject(Resource.msg('msg.mail.added.tocart.product.subject', 'cart', null));
+    mail.setContent(emailContent);
+    mail.send();
 
     return next();
 });
