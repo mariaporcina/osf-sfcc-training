bee3ee65eb9d4c3484913ccef71ef6b352aaee3e
maria-osfdigital <maria.porcina@osf.digital>
2022-04-07 18:21:35 -0300

    finishing exercise
    - adding new entries the bundle resources file
    - setting up logout functionality
    - finish setting up the login functionality
    - retrieving and displaying user's data

---
+++ cartridges/app_storefront_base/cartridge/client/default/js/facebook/loginSetup.js
@@ -0,0 +1,79 @@
+'use strict';
+
+const test = () => {
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '391956239599920',
+      xfbml      : true,
+      version    : 'v13.0'
+    });
+    FB.AppEvents.logPageView();
+  };
+
+  (function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+
+  $(document).on("click", "button.login-in-button", function() {
+    FB.getLoginStatus(function(response) {
+      if(response.status !== 'connected'){
+        loginIn();
+      } else {
+        getUserProfile();
+      }
+    });
+
+    $(document).on("click", "button.logout-button", function() {
+      logout();
+    });
+
+  });
+}
+
+const getUserProfile = () => {
+  FB.api('/me', {fields: 'id,name,email'}, response => {
+    var data = {
+      id: response.id,
+      name: response.name,
+      email: response.email
+    }
+
+    FB.api('/me/picture?redirect=false&width=300', response => {
+      data.imageUrl = response.data.url;
+      renderUserProfile(data);
+    });
+  });
+}
+
+const renderUserProfile = data => {
+  $('h5.card-title').text(data.name);
+  $('h6.card-subtitle').text(data.id);
+  $('p.card-text').text(data.email);
+
+  $('img.card-img-top').attr('src', data.imageUrl);
+  $('img.card-img-top').attr('alt', data.name);
+}
+
+const loginIn = () => {
+  FB.login(response => {
+    if (response.authResponse) {
+      getUserProfile();
+    } else {
+      console.log('User cancelled login or did not fully authorize.');
+    }
+  }, {scope: 'public_profile,email'});
+}
+
+const logout = () => {
+  FB.logout(response => {
+    console.log('You were logged out from Facebook. To login again click on the "Authenticate" button.')
+  });
+}
+
+module.exports = () => {
+  test()
+}
--- cartridges/app_storefront_base/cartridge/client/default/js/facebookLogin.js
+++ cartridges/app_storefront_base/cartridge/client/default/js/facebookLogin.js
@@ -3,5 +3,5 @@
 var processInclude = require('./util');
 
 $(document).ready(function () {
-    processInclude(require('./facebook/login/loginSetup'));
+    processInclude(require('./facebook/loginSetup'));
 });
