2ad133df923226de6b3afd7168da53600461712e
maria-osfdigital <maria.porcina@osf.digital>
2022-05-05 16:11:46 -0300

    setting up controller to retrieve badge's information and send to the template

---
+++ cartridges/app_training/cartridge/controllers/Search.js
@@ -0,0 +1,31 @@
+
+'use strict';
+
+var server = require('server');
+
+server.extend(module.superModule);
+
+server.append('Show', function(req, res, next) {
+    var CatalogMgr = require('dw/catalog/CatalogMgr');
+    var searchHelper = require('*/cartridge/scripts/helpers/searchHelpers');
+
+    var viewData = res.getViewData();
+
+    var result = searchHelper.search(req, res);
+
+    var categoryId = result.productSearch.category.id;
+    var category = CatalogMgr.getCategory(categoryId);
+
+    var badgeConfiguration = {
+        backgroundColor: category.custom.badgeBackgroundColor,
+        textColor: category.custom.badgeTextColor,
+        text: category.custom.badgeTextValue
+    };
+
+    viewData.badge = badgeConfiguration;
+    res.setViewData(viewData);
+
+    return next();
+});
+
+module.exports = server.exports();
