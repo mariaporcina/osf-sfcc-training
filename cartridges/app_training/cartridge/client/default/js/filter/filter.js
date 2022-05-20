b8c7b35883320383bf0a882b090b80000d90bce7
maria-osfdigital <maria.porcina@osf.digital>
2022-05-20 15:46:52 -0300

    finishing exercise
    - finishing setting up controller's routes
    - setting up functionality for opening the modal and to display the searched key word
    - finish setting up the modal's template

--- cartridges/app_training/cartridge/client/default/js/filter/filter.js
+++ cartridges/app_training/cartridge/client/default/js/filter/filter.js
@@ -1,3 +1,50 @@
 'use strict';
 
-// const
+const showResults = () => {
+    const form = $('form.filterForm');
+    const okButton = $('button.filter-modal-button');
+    const filterInput = $('input.filterInput');
+
+    form.on('submit', function(event) {
+        event.preventDefault();
+
+        $.ajax({
+            url: form.attr('action'),
+            method: "GET",
+            success: function(data) {
+                const modal = $(`
+                    <div class="modal fade my-exercise-modal" id="myModal" role="dialog">
+                        ${data}
+                    </div>
+                `);
+
+                modal.on('hidden.bs.modal', event => {
+                    $(event.target).remove();
+                });
+
+                const inputValue = filterInput.val();
+
+                modal.on('shown.bs.modal', () => {
+                    const span = $('span.keyWord');
+                    span.text(inputValue);
+                });
+
+                modal.modal();
+            },
+            error: function(err) {
+                console.error(err);
+            }
+        });
+        return false;
+    });
+
+    $(document).on('click', okButton, event => {
+        if($(event.target).prop('tagName') === "BUTTON"){
+            $('#myModal').modal('hide');
+        }
+    });
+}
+
+module.exports = () => {
+    showResults();
+}
--- cartridges/app_training/cartridge/controllers/Filter.js
+++ cartridges/app_training/cartridge/controllers/Filter.js
@@ -3,7 +3,17 @@
 var server = require('server');
 
 server.get('Show', function(req, res, next) {
-    res.render('filter/filterForm.isml');
+    var URLUtils = require('dw/web/URLUtils');
+
+    res.render('filter/filterForm.isml', {
+        actionUrl: URLUtils.url('Filter-ShowModal').toString()
+    });
+    return next();
+});
+
+server.get('ShowModal', function(req, res, next) {
+    res.render('filter/components/resultModal.isml');
+
     return next();
 });
 
